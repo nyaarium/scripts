7a730f0531997a183a3c036f0eac67a0d4b3c098
Nyaarium <project@nyaarium.com>
2024-07-26 14:06:20 -0700

    Option to skip own common libs

--- approve-dependabot.sh
+++ approve-dependabot.sh
@@ -3,6 +3,22 @@
 source ~/.env
 
 
+while true; do
+    echo ""
+    echo -n "❓ Approve our own common libs? Y/N: "
+    read confirmation
+    if [ "$confirmation" = "y" ] || [ "$confirmation" = "Y" ]; then
+      approve_own="y"
+        break
+    elif [ "$confirmation" = "n" ] || [ "$confirmation" = "N" ]; then
+      approve_own="n"
+        break
+    else
+      echo "Expected a Y/N answer."
+    fi
+done
+
+
 # Check if GitHub CLI is ready
 command -v gh >/dev/null 2>&1 && gh auth status >/dev/null 2>&1
 gh_ready_status=$?
@@ -25,6 +41,14 @@ echo "$gh_prs" | while read -r pr; do
   pr_author=$(echo "$pr" | jq -r '.author.login')
 
   if [ "$pr_author" == "app/dependabot" ]; then
+    pr_title=$(echo "$pr" | jq -r '.title')
+
+    if [ "$approve_own" == "n" ]; then
+      [[ "$pr_title" == "Bump js-common from" ]] && continue
+      [[ "$pr_title" == "Bump next-common from" ]] && continue
+      [[ "$pr_title" == "Bump react-controlled-input from" ]] && continue
+    fi
+  
     gh pr merge "$pr_number" --auto -m
     gh pr review --approve "$pr_number"
   fi
